9e1855da67e517a986572f50717b3a098a1d9f6b
Devin Brown <102552455+DevinBrown11@users.noreply.github.com>
2024-05-18 22:26:02 -0500

    Update Dog.java
    This is the file in which the field and Constructor information is being called from.

--- Dog.java
+++ Dog.java
@@ -1,9 +1,12 @@
+// The file in which we are calling the fields and Constructor method from. 
+// Remember you can create multiple Constructor Methods only if you have different parameters.
 public class Dog {
     int age;
     String name;
     
     
     public Dog(String name , int age) {
+        // this is able to set the fields name and age as the same value as the parameters name and age.
         this.name = name;
         this.age = age;
         
